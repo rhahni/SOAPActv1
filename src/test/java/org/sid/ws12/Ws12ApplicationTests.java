2eb5fabe0838896efcf90ff52310b9e6078bf879
rhahni <oussama.ord@gmail.com>
2023-12-06 14:20:59 +0100

    first commit

---
+++ src/test/java/org/sid/ws12/Ws12ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.ws12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
